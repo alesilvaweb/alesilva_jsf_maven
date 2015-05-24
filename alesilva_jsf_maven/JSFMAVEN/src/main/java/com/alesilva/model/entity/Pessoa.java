5ef1deed0d08ce2cb856d447d164a38632d8d4ad
Alessandro <Alessandro@Acer>
2015-05-23 23:19:40 -0300

    Entidade Sexo

--- alesilva_jsf_maven/JSFMAVEN/src/main/java/com/alesilva/model/entity/Pessoa.java
+++ alesilva_jsf_maven/JSFMAVEN/src/main/java/com/alesilva/model/entity/Pessoa.java
@@ -8,8 +8,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import org.hibernate.annotations.ForeignKey;
 
 
 
@@ -38,6 +40,10 @@ public class Pessoa implements Serializable {
    @Column(name = "dt_cadastro")
    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
    private Date dataCadastro;
+   
+   @ManyToOne(optional = false)
+   @ForeignKey(name = "PessoaSexo")
+   private Pessoa pessoa;
 
     public Pessoa() {
     }
